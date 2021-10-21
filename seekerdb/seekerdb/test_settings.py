b783e038acecd8b2661c01fc54efcba89986ee49
stewartad <austendean@gmail.com>
2021-10-20 17:31:59 -0700

    remove unused key

--- seekerdb/seekerdb/test_settings.py
+++ seekerdb/seekerdb/test_settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vzxr3=wax1$4ad2z^h_tpue^&zf20cqo=l_003dk&+ruk)qpy3'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
